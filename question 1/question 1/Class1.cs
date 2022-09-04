7e6f1c23c45a92edaa389fc997beccd516a5dbcd
mohamedissam6 <112808440+mohamedissam6@users.noreply.github.com>
2022-09-04 20:39:27 +0200

    Add files via upload

---
+++ question 1/question 1/Class1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace question_1
+{
+    internal class Time
+    {
+        public int hour;
+        public int minute;
+        public int second;
+
+        public Time()
+        {
+            Time time = new Time();
+        }
+
+        public Time(int hour, int minute, int second)
+        {
+            this.hour = hour;
+            this.minute = minute;
+            this.second = second;
+        }
+
+        public static void print12(Time time)
+        {
+            int x = time.hour;
+            if (time.hour > 12) { 
+                x-=12;
+            Console.WriteLine("{0} : {1} : {2} PM",x,time.minute,time.second);
+        }
+            else
+            {
+            Console.WriteLine("{0} : {1} : {2} AM", x, time.minute, time.second);
+            }
+        }
+
+        public static void print24(Time time)
+        {
+            Console.WriteLine("{0} : {1} : {2} ",time.hour,time.minute,time.second); 
+        }
+
+    }
+}
---
+++ question 1/question 1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace question_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Time time = new Time(10, 30, 45);
+            Time.print12(time);
+            Time.print24(time);
+            Console.ReadLine();
+
+        }
+    }
+}
